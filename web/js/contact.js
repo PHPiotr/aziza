e91ebf702c9b3e0f4473c47ff601818d8d971251
Piotr Kowalski <pkowalski@madbook.local>
2017-02-27 03:17:57 +0100

    Display popover content on top - fix for mobile.

--- web/js/contact.js
+++ web/js/contact.js
@@ -91,20 +91,20 @@ var Contact = React.createClass({
                                 ),
                         React.createElement('form', {onSubmit: this.onSubmitHandler, className: 'form-horizontal'},
                                 React.createElement('div', {className: 'form-group'},
-                                        React.createElement('label', {'data-placement': 'left', 'data-content': this.state.phoneError, htmlFor: 'phone', className: 'phone-popover col-sm-offset-4 col-sm-1 control-label'}, 'Telefon'),
+                                        React.createElement('label', {'data-placement': 'top', 'data-content': this.state.phoneError, htmlFor: 'phone', className: 'phone-popover col-sm-offset-4 col-sm-1 control-label'}, 'Telefon'),
                                         React.createElement('div', {className: 'col-sm-6'},
                                                 React.createElement('input', {onChange: this.onChangeHandler, value: this.state.phone, type: 'text', maxLength: '15', className: 'form-control', id: 'phone', name: 'phone'})
                                                 )
                                         ),
                                 React.createElement('div', {className: 'form-group'},
-                                        React.createElement('label', {'data-placement': 'left', 'data-content': this.state.emailError, htmlFor: 'email', className: 'email-popover col-sm-offset-4 col-sm-1 control-label'}, 'Email'),
+                                        React.createElement('label', {'data-placement': 'top', 'data-content': this.state.emailError, htmlFor: 'email', className: 'email-popover col-sm-offset-4 col-sm-1 control-label'}, 'Email'),
                                         React.createElement('div', {className: 'col-sm-6'},
                                                 React.createElement('input', {onChange: this.onChangeHandler, value: this.state.email, type: 'text', className: 'form-control', id: 'email', name: 'email'})
                                                 )
                                         ),
                                 React.createElement('div', {className: 'form-group'},
                                         React.createElement('div', {className: 'col-sm-offset-4 col-sm-7'},
-                                                React.createElement('textarea', {'data-placement': 'left', 'data-content': this.state.messageError, onChange: this.onChangeHandler, value: this.state.message, className: 'message-popover form-control', rows: 6, id: 'message', name: 'message'})
+                                                React.createElement('textarea', {'data-placement': 'top', 'data-content': this.state.messageError, onChange: this.onChangeHandler, value: this.state.message, className: 'message-popover form-control', rows: 6, id: 'message', name: 'message'})
                                                 )
                                         ),
                                 React.createElement('div', {className: 'form-group submit'},
